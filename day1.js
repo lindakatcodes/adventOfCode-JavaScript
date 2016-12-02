c2000cccfe231e4f827855078df5c717b9202dc8
lindakatcodes <lindakt16@gmail.com>
2016-12-02 00:26:10 -0600

    Fixed so parts 1 & 2 pass!

--- day1.js
+++ day1.js
@@ -1,11 +1,12 @@
-//Code ideas borrowed from reddit users u/LieutenantSwr2d2 and u/tuxitop
+//Code ideas borrowed/initiated from reddit users u/LieutenantSwr2d2 and u/tuxitop; 
+//thanks for debugging assistance u/bbatsell, u/msully4321, & u/mrbkap!
 
-var moves = ['R2', 'L1', 'R2', 'R1', 'R1', 'L3', 'R3', 'L5', 'L5', 'L2', 'L1', 'R4', 'R1', 'R3', 'L5', 'L5', 'R3', 'L4', 'L4', 'R5', 'R4', 'R3', 'L1', 'L2', 'R5', 'R4', 'L2', 'R1', 'R4', 'R4', 'L2', 'L1', 'L1', 'R190', 'R3', 'L4', 'R52', 'R5', 'R3', 'L5', 'R3', 'R2', 'R1', 'L5', 'L5', 'L4', 'R2', 'L3', 'R3', 'L1', 'L3', 'R5', 'L3', 'L4', 'R3', 'R77', 'R3', 'L2', 'R189', 'R4', 'R2', 'L2', 'R2', 'L1', 'R5', 'R4', 'R4', 'R2', 'L2', 'L2', 'L5', 'L1', 'R1', 'R2', 'L3', 'L4', 'L5', 'R1', 'L1', 'L2', 'L2', 'R2', 'L3', 'R3', 'L4', 'L1', 'L5', 'L4', 'L4', 'R3', 'R5', 'L2', 'R4', 'R5', 'R3', 'L2', 'L2', 'L4', 'L2', 'R2', 'L5', 'L4', 'R3', 'R1', 'L2', 'R2', 'R4', 'L1', 'L4', 'L4', 'L2', 'R2', 'L4', 'L1', 'L1', 'R4', 'L1', 'L3', 'L2', 'L2', 'L5', 'R5', 'R2', 'R5', 'L1', 'L5', 'R2', 'R4', 'R4', 'L2', 'R5', 'L5', 'R5', 'R5', 'L4', 'R2', 'R1', 'R1', 'R3', 'L3', 'L3', 'L4', 'L3', 'L2', 'L2', 'L2', 'R2', 'L1', 'L3', 'R2', 'R5', 'R5', 'L4', 'R3', 'L3', 'L4', 'R2', 'L5', 'R5'];
+var moves = ['R2', 'L1', 'R2', 'R1', 'R1', 'L3', 'R3', 'L5', 'L5', 'L2', 'L1', 'R4', 'R1', 'R3', 'L5', 'L5', 'R3', 'L4', 'L4', 'R5', 'R4', 'R3', 'L1', 'L2', 'R5', 'R4', 'L2', 'R1', 'R4', 'R4', 'L2', 'L1', 'L1', 'R190', 'R3', 'L4', 'R52', 'R5', 'R3', 'L5', 'R3', 'R2', 'R1', 'L5', 'L5', 'L4', 'R2', 'L3', 'R3', 'L1', 'L3', 'R5', 'L3', 'L4', 'R3', 'R77', 'R3', 'L2', 'R189', 'R4', 'R2', 'L2', 'R2', 'L1', 'R5', 'R4', 'R4', 'R2', 'L2', 'L2', 'L5', 'L1', 'R1', 'R2', 'L3', 'L4', 'L5', 'R1', 'L1', 'L2', 'L2', 'R2', 'L3', 'R3', 'L4', 'L1', 'L5', 'L4', 'L4', 'R3', 'R5', 'L2', 'R4', 'R5', 'R3', 'L2', 'L2', 'L4', 'L2', 'R2', 'L5', 'L4', 'R3', 'R1', 'L2', 'R2', 'R4', 'L1', 'L4', 'L4', 'L2', 'R2', 'L4', 'L1', 'L1', 'R4', 'L1', 'L3', 'L2', 'L2', 'L5', 'R5', 'R2', 'R5', 'L1', 'L5', 'R2', 'R4', 'R4', 'L2', 'R5', 'L5', 'R5', 'R5', 'L4', 'R2', 'R1', 'R1', 'R3', 'L3', 'L3', 'L4', 'L3', 'L2', 'L2', 'L2', 'R2', 'L1', 'L3', 'R2', 'R5', 'R5', 'L4', 'R3', 'L3', 'L4', 'R2', 'L5', 'R5']; 
 
 var x = 0, y = 0;
 var direction = 0; //0 north, 1 east, 2 south, 3 west
 var position = [x, y];
-var spots = ['0,0'];
+var spots = [];
 var twice;
 var foundTwice = false;
 
@@ -14,58 +15,65 @@ moves.forEach(function (instruction) {
     var n = parseInt(instruction.slice(1));
     switch (direction) {
         case 0:
-            y += n;
             if (foundTwice === false) {
-                position = [x, y];
-                if (spots.indexOf(position.toString()) === -1) {
-                    spots.push(position.toString());
-                } else {
-                    foundTwice = true;
-                    twice = position;
+                for (var i = 0; i < n; i++) {
+                    position = [x, y + i];
+                    if (spots.indexOf(position.toString()) === -1) {
+                        spots.push(position.toString());
+                    } else {
+                        foundTwice = true;
+                        twice = position;
+                    }
                 }
             }
+            y += n;
             break;
         case 1:
-            x += n;
             if (foundTwice === false) {
-                position = [x, y];
-                if (spots.indexOf(position.toString()) === -1) {
-                    spots.push(position.toString());
-                } else {
-                    foundTwice = true;
-                    twice = position;
+                for (var i = 0; i < n; i++) {
+                    position = [x + i, y];
+                    if (spots.indexOf(position.toString()) === -1) {
+                        spots.push(position.toString());
+                    } else {
+                        foundTwice = true;
+                        twice = position;
+                    }
                 }
             }
+            x += n;
             break;
         case 2:
-            y -= n;
             if (foundTwice === false) {
-                position = [x, y];
-                if (spots.indexOf(position.toString()) === -1) {
-                    spots.push(position.toString());
-                } else {
-                    foundTwice = true;
-                    twice = position;
+                for (var i = 0; i < n; i++) {
+                    position = [x, y - i];
+                    if (spots.indexOf(position.toString()) === -1) {
+                        spots.push(position.toString());
+                    } else {
+                        foundTwice = true;
+                        twice = position;
+                    }
                 }
             }
+            y -= n;
             break;
         case 3:
-            x -= n;
             if (foundTwice === false) {
-                position = [x, y];
-                if (spots.indexOf(position.toString()) === -1) {
-                    spots.push(position.toString());
-                } else {
-                    foundTwice = true;
-                    twice = position;
+                for (var i = 0; i < n; i++) {
+                    position = [x - i, y];
+                    if (spots.indexOf(position.toString()) === -1) {
+                        spots.push(position.toString());
+                    } else {
+                        foundTwice = true;
+                        twice = position;
+                    }
                 }
             }
+            x -= n;
             break;
     }
 })
 
 
 console.log(Math.abs(x) + Math.abs(y));
-console.log(spots);
 console.log(twice);
 console.log(Math.abs(twice[0] + Math.abs(twice[1])));
