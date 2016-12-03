c68ec37041e2bba5fe07436a16611bcdb4937401
lindakatcodes <lindakt16@gmail.com>
2016-12-02 23:29:06 -0600

    Added comments

--- day1.js
+++ day1.js
@@ -28,6 +28,7 @@ How many blocks away is the first location you visit twice?
 
 var moves = ['R2', 'L1', 'R2', 'R1', 'R1', 'L3', 'R3', 'L5', 'L5', 'L2', 'L1', 'R4', 'R1', 'R3', 'L5', 'L5', 'R3', 'L4', 'L4', 'R5', 'R4', 'R3', 'L1', 'L2', 'R5', 'R4', 'L2', 'R1', 'R4', 'R4', 'L2', 'L1', 'L1', 'R190', 'R3', 'L4', 'R52', 'R5', 'R3', 'L5', 'R3', 'R2', 'R1', 'L5', 'L5', 'L4', 'R2', 'L3', 'R3', 'L1', 'L3', 'R5', 'L3', 'L4', 'R3', 'R77', 'R3', 'L2', 'R189', 'R4', 'R2', 'L2', 'R2', 'L1', 'R5', 'R4', 'R4', 'R2', 'L2', 'L2', 'L5', 'L1', 'R1', 'R2', 'L3', 'L4', 'L5', 'R1', 'L1', 'L2', 'L2', 'R2', 'L3', 'R3', 'L4', 'L1', 'L5', 'L4', 'L4', 'R3', 'R5', 'L2', 'R4', 'R5', 'R3', 'L2', 'L2', 'L4', 'L2', 'R2', 'L5', 'L4', 'R3', 'R1', 'L2', 'R2', 'R4', 'L1', 'L4', 'L4', 'L2', 'R2', 'L4', 'L1', 'L1', 'R4', 'L1', 'L3', 'L2', 'L2', 'L5', 'R5', 'R2', 'R5', 'L1', 'L5', 'R2', 'R4', 'R4', 'L2', 'R5', 'L5', 'R5', 'R5', 'L4', 'R2', 'R1', 'R1', 'R3', 'L3', 'L3', 'L4', 'L3', 'L2', 'L2', 'L2', 'R2', 'L1', 'L3', 'R2', 'R5', 'R5', 'L4', 'R3', 'L3', 'L4', 'R2', 'L5', 'R5']; 
 
+//coords, curr direction & position, array of visited spots, and vars to check for first location visited twice
 var x = 0, y = 0;
 var direction = 0; //0 north, 1 east, 2 south, 3 west
 var position = [x, y];
@@ -36,11 +37,12 @@ var twice;
 var foundTwice = false;
 
 moves.forEach(function (instruction) {
-    direction = (direction + (instruction[0] === 'R' ? 1 : -1) + 4) % 4;
-    var n = parseInt(instruction.slice(1));
+    direction = (direction + (instruction[0] === 'R' ? 1 : -1) + 4) % 4; //if instruc. is R go 1, else go -1; +4 %4 to keep direc. in range
+    var n = parseInt(instruction.slice(1)); //number of steps to move
     switch (direction) {
-        case 0:
-            if (foundTwice === false) {
+        //goes through each direction: if haven't found twice var, goes through each move adding or subtracting to x or y depending on direction; if not in spots, adds to spots, otherwise you've found twice and set it - same for all 4 cases
+        case 0: //N
+            if (foundTwice === false) { 
                 for (var i = 0; i < n; i++) {
                     position = [x, y + i];
                     if (spots.indexOf(position.toString()) === -1) {
@@ -53,7 +55,7 @@ moves.forEach(function (instruction) {
             }
             y += n;
             break;
-        case 1:
+        case 1: //E
             if (foundTwice === false) {
                 for (var i = 0; i < n; i++) {
                     position = [x + i, y];
@@ -67,7 +69,7 @@ moves.forEach(function (instruction) {
             }
             x += n;
             break;
-        case 2:
+        case 2: //S
             if (foundTwice === false) {
                 for (var i = 0; i < n; i++) {
                     position = [x, y - i];
@@ -81,7 +83,7 @@ moves.forEach(function (instruction) {
             }
             y -= n;
             break;
-        case 3:
+        case 3: //W
             if (foundTwice === false) {
                 for (var i = 0; i < n; i++) {
                     position = [x - i, y];
@@ -98,7 +100,7 @@ moves.forEach(function (instruction) {
     }
 })
 
-
+//shows x+y for number of blocks away from start, plus twice visited location
 console.log(Math.abs(x) + Math.abs(y));
 console.log(twice);
 console.log(Math.abs(twice[0] + Math.abs(twice[1])));
