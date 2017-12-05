2ca3509815dd04e0cc808c08e9f842340e9d3be8
lindakatcodes <lindakt16@gmail.com>
2017-12-04 22:11:24 -0600

    Day 3 p2 img added, day 4 input included

--- skeleton.js
+++ skeleton.js
@@ -1,16 +1,1 @@
-const http = require('http');
 
-http.get('http://adventofcode.com/2017/day/2/input', (response) => {
-  const input = [];
-  response.on('data', (chunk) => {
-    input.push(chunk.toString());
-  });
-  response.on('end', () => {
-    try {
-      // const parsedInput = JSON.parse(input);
-      console.log(input);
-    } catch (e) {
-      console.log(`Error: ${e.message}`);
-    }
-  });
-});
