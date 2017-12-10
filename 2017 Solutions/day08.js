e653df2800b5c63db264c5a9495c5764f0b5555b
Kat <lindakt16@gmail.com>
2017-12-10 12:54:32 -0600

    Day 8 solved! Hallelujiah, a quicker one! :)

--- 2017 Solutions/day08.js
+++ 2017 Solutions/day08.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 
-// Change number on input file before use
 const data = fs.readFileSync('../2017 Solutions/inputs/day08Input.txt').toString();
 
-// Need an array of strings?
 const input = data.split('\r\n');
 
 let instruct = [];
 let registers = [];
 let unique = [];
+let overallHighest = 0;
 
 for (let i = 0; i < input.length; i++) {
     instruct.push(input[i].split(' '));
@@ -25,8 +24,28 @@ for (let j = 0; j < instruct.length; j++) {
     let reg = unique.indexOf(instruct[j][4]);
     let condition = `${registers[reg].value} ${instruct[j][5]} ${instruct[j][6]}`;
 
-    if (condition.valueOf()) {
-        console.log('true');
+    if (eval(condition)) {
+        let regtoChange = unique.indexOf(instruct[j][0]);
+        let amttoChange = parseInt(instruct[j][2], 10);
+
+        if (instruct[j][1] === 'inc') {
+            registers[regtoChange].value += amttoChange;
+        } else if (instruct[j][1] === 'dec') {
+            registers[regtoChange].value -= amttoChange;
+        }
+
+        if (registers[regtoChange].value > overallHighest) {
+            overallHighest = registers[regtoChange].value;
+        }
     }
 }
 
+let largest = 0;
+
+for (let i = 0; i < registers.length; i++) {
+    if (registers[i].value > largest) {
+        largest = registers[i].value;
+    }
+}
+
+console.log(`largest at end: ${largest}; largest seen: ${overallHighest}`);
