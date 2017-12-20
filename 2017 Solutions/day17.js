4e3e5893af386e457591007243b923e3a9729c1f
Kat <lindakt16@gmail.com>
2017-12-19 21:39:58 -0600

    Day 17 done!

--- 2017 Solutions/day17.js
+++ 2017 Solutions/day17.js
@@ -1,6 +1,9 @@
+// No special shoutout for this one - just general talk about how to get around the buffer manipulation
+
+// part 1
 const steps = 354;
 
-const times = 50000000;
+let times = 2017;
 let buffer = [0];
 let current = buffer[0];
 
@@ -11,6 +14,23 @@ for (let i = 1; i <= times; i++) {
     current = buffer[endIndex];
 }
 
-// let spot = buffer.indexOf(2017);
-// console.log(buffer[spot + 1]);
-console.log(buffer[1]);
+let spot = buffer.indexOf(2017);
+console.log(buffer[spot + 1]);
+
+// part 2
+times = 50000000;
+buffer = 1;
+current = 1;
+let oneSpot = [];
+
+for (let i = 1; i <= times; i++) {
+    let moveIndex = current + steps;
+    current = (moveIndex % buffer) + 1;
+    buffer++;
+
+    if (current === 1) {
+        oneSpot.push(buffer - 1);
+    }
+}
+
+console.log(oneSpot[oneSpot.length - 1]);
