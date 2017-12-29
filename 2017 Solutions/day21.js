b821fc01618cfe91ea8250f0e3c4fe6c293bfefa
lindakatcodes <lindakt16@gmail.com>
2017-12-28 22:05:36 -0600

    Restarting day 21

--- 2017 Solutions/day21.js
+++ 2017 Solutions/day21.js
@@ -7,42 +7,9 @@ const rules = data.split('\r\n').map((rule) => {
     return [broken[0].trim(), broken[1].trim()];
 });
 
-let program = '#..#/..../..../#..#'.split('/');
+let program = '.#./..#/###'.split('/');
 let size = program.length;
 let on = 0;
-
-function splitGroups(grid, num) {
-    let newPairs = [];
-
-    for (let i = 0; i < grid.length; i++) {
-        let front = grid[i].slice(0, num);
-        let back = grid[i].slice(num);
-        grid.splice(i, 1, [front, back]);
-    }
-
-    return grid;
-}
-
-function matchGroup(group, num) {
-    let change = '';
-    if (num === 2) {
-        for (let a = 0; a < 1; a++) {
-            let set = rules[a][0];
-            if (group === set) {
-                change = rules[a][1];
-            }
-        }
-    } else if (num === 3) {
-        for (let b = 1; b < rules.length; b++) {
-            let set = rules[b][0];
-            if (group === set) {
-                change = rules[b][1];
-            }
-        }
-    }
-
-    return change;
-}
 
 // for each iteration
 for (let i = 0; i < 2; i++) {
