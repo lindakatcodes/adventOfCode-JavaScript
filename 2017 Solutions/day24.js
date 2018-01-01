83988f661956f7957ed890f9a99b4df167556f03
lindakatcodes <lindakt16@gmail.com>
2018-01-01 15:07:31 -0600

    Still no luck on day 24

--- 2017 Solutions/day24.js
+++ 2017 Solutions/day24.js
@@ -4,82 +4,83 @@ const data = fs.readFileSync('../2017 Solutions/inputs/day24Input.txt').toString
 
 const input = data.split('\r\n');
 
+// My attempt - couldn't get it to give me the right answer for the problem and was taking too long, so had to give up in order to move on
+
 let starters = input.filter(piece => piece.match(/^0\/|\/0$/));
 let bridges = [];
+let maxLen = 0;
 
-function build(point, arr) {
-    let star = point.split('/');
-    let end = true;
+function getMatches(starter, connection, arr) {
+    // find all the pieces that can connect with the connector
     let matches = [];
 
     for (let i = 0; i < input.length; i++) {
-        let curr = input[i].split('/');
+        let curr = input[i].split('/').map(x => parseInt(x, 10));
 
-        if (input[i] === point || arr.includes(input[i])) {
+        // if the curr point is the startpoint or already in the current tempStore, skip it
+        if (input[i] === starter || arr.includes(input[i])) {
             continue;
         }
 
-        if (star[1] === curr[0] || star[1] === curr[1]) {
-            matches.push(curr);
-            end = false;
+        if (connection === curr[0] || connection === curr[1]) {
+            matches.push(curr.join('/'));
         }
     }
+    arr.push(starter);
+    return matches;
+}
 
-    if (matches.length > 0) {
-        matches.forEach((pt) => {
-            let last = arr[arr.length - 1].split('/')[1];
-            if (!arr.includes(pt.join('/'))) {
-                if (pt[0] === last || pt[1] === last) {
-                    arr.push(pt.join('/'));
-                    build(pt.join('/'), arr);
-                }
-                let newArr = arr.slice(0);
-                newArr.pop();
-                newArr.push(pt.join('/'));
-                build(pt.join('/'), newArr);
-            }
-        });
+function build(start, connect, tempArr) {
+    let pushed = false;
+    // get starting point
+    let startPoint = start.split('/').map(x => parseInt(x, 10));
+    // get it's connector, save that
+    let connector = startPoint[0] === connect ? startPoint[1] : startPoint[0];
+    // set a build temp array, so getMatches can access it
+    let buildArr = tempArr;
+    let toCheck = getMatches(startPoint.join('/'), connector, buildArr);
+
+    for (let j = 0; j < toCheck.length; j++) {
+        let newArr = buildArr.slice();
+        if (pushed) {
+            newArr.pop();
+        }
+        build(toCheck[j], connector, newArr);
     }
 
-    if (end) {
-        if (!bridges.includes(arr)) {
-            bridges.push(arr);
-        }
-        matches = [];
+    let prevArr = [];
+    if (bridges.length < 1) {
+        prevArr = '';
     } else {
-        build(arr[arr.length - 1], arr);
+        prevArr = bridges[bridges.length - 1].join('');
     }
-}
+    let currArr = buildArr.join('');
 
-function testAll() {
-    for (let a = 0; a < starters.length; a++) {
-        let startPoint = starters[a];
-        let nextNum = startPoint.split('/')[1];
-        let seconds = input.filter(piece => piece.match(nextNum));
-        seconds.splice(seconds.indexOf(startPoint), 1);
-        let progress = [startPoint];
-        for (let j = 0; j < seconds.length; j++) {
-            let next = seconds[j];
-            progress.push(next);
-            build(next, progress);
-            progress = [startPoint];
+    if (!bridges.includes(buildArr) && !prevArr.includes(currArr)) {
+        let total = getTotal(buildArr);
+        if (total > maxLen) {
+            maxLen = total;
         }
+        bridges.push(buildArr);
+        pushed = true;
     }
 }
 
-testAll();
-
-let maxLen = 0;
-
-for (let i = 0; i < bridges.length; i++) {
-    let current = bridges[i]
+function getTotal(bridge) {
+    let nums = bridge
         .map(num => num.split('/'))
         .reduce((a, b) => a.concat(b))
         .map(num => parseInt(num, 10));
-    let total = current.reduce(((a, b) => a + b), 0);
-    if (total > maxLen) {
-        maxLen = total;
-    }
+    return nums.reduce(((a, b) => a + b), 0);
 }
 
+// for each starter
+for (let a = 0; a < starters.length; a++) {
+    // make a temp array, to store each bridge as I build it
+    let tempStore = [];
+    build(starters[a], 0, tempStore);
+}
+
+console.log(bridges);
 console.log(maxLen);
+
