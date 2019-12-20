0123108c941e9831710b68c57b6673cac2572942
Lindakat <lindakt16@gmail.com>
2019-12-20 09:21:31 -0600

    day 14 started

--- 2019 Solutions/day14.js
+++ 2019 Solutions/day14.js
@@ -7,5 +7,15 @@ const data = fs.readFileSync('../2019 Solutions/inputs/day14input.txt').toString
 const input = data.split('\r\n');
 const ingredients = input.map(row => row.split(/,|=>/));
 
-let oreQty = 0;
+// console.log(ingredients);
+
+// find the recipe for fuel
+let fuelIndex = 0;
+for (let i = 0; i < ingredients.length; i++) {
+  for (let j = 0; j < ingredients[i].length; j++) {
+    if (ingredients[i][j].includes('1 FUEL')) {
+      fuelIndex = i;
+    }
+  }
+}
 
