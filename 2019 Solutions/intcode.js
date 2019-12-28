eff6aad9001dc776a994dfebdeccf1598db8f260
Lindakat <lindakt16@gmail.com>
2019-12-28 01:23:58 -0600

    day17 p1 done; struggling on p2

--- 2019 Solutions/intcode.js
+++ 2019 Solutions/intcode.js
@@ -2,44 +2,44 @@
 let relBase = 0;
 
 // opcode 1 - get values at position 1&2 right after code, add together, store in position 3
-function opcode1 (a, b, c, p) {
-  let valA = ptest(p[0], a, 'read');
-  let valB = ptest(p[1], b, 'read');
-  let valC = ptest(p[2], c, 'write');
+function opcode1 (a, b, c, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
+  let valB = ptest(p[1], b, 'read', program);
+  let valC = ptest(p[2], c, 'write', program);
 
-  inputCopy[valC] = valA + valB;
+  program[valC] = valA + valB;
   // console.log(`op1: ${valA} + ${valB} = ${valA + valB}`)
 }
 
 // opcode 2 - get values at position 1&2 right after code, multiply, store in position 3
-function opcode2 (a, b, c, p) {
-  let valA = ptest(p[0], a, 'read');
-  let valB = ptest(p[1], b, 'read');
-  let valC = ptest(p[2], c, 'write');
+function opcode2 (a, b, c, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
+  let valB = ptest(p[1], b, 'read', program);
+  let valC = ptest(p[2], c, 'write', program);
 
-  inputCopy[valC] = valA * valB;
+  program[valC] = valA * valB;
   // console.log(`op2: ${valA} * ${valB} = ${valA * valB}`)
 }
 
 // opcode 3 - takes an input and stores in position 1
-function opcode3 (iv, s, p) {
-  let pos = ptest(p[0], s, 'write');
+function opcode3 (iv, s, p, program) {
+  let pos = ptest(p[0], s, 'write', program);
 
-  inputCopy[pos] = iv;
+  program[pos] = iv;
   // console.log(`op3: putting ${iv} into spot ${pos}, mode ${p[0]}`)
 }
 
 // opcode 4 - outputs value at position 1
-function opcode4 (s, p) {
-  let val = ptest(p[0], s, 'read');
+function opcode4 (s, p, program) {
+  let val = ptest(p[0], s, 'read', program);
   // console.log(`op4: outputting ${val} at ${relBase + s}`)
   return val;
 }
 
 // opcode 5 - if position 1 != 0, changes i to position 2; otherwise, does nothing
-function opcode5 (a, b, inp, p) {
-  let valA = ptest(p[0], a, 'read');
-  let valB = ptest(p[1], b, 'read');
+function opcode5 (a, b, inp, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
+  let valB = ptest(p[1], b, 'read', program);
 
   if (valA !== 0) {
     inp = valB;
@@ -49,9 +49,9 @@ function opcode5 (a, b, inp, p) {
 }
 
 // opcode 6 - if position 1 == 0, changes i to position 2; otherwise, does nothing
-function opcode6 (a, b, inp, p) {
-  let valA = ptest(p[0], a, 'read');
-  let valB = ptest(p[1], b, 'read');
+function opcode6 (a, b, inp, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
+  let valB = ptest(p[1], b, 'read', program);
 
   if (valA === 0) {
     inp = valB;
@@ -62,48 +62,48 @@ function opcode6 (a, b, inp, p) {
 }
 
 // opcode 7 - if position 1 < position 2, position 3 is set to 1; otherwise, it's set to 0
-function opcode7 (a, b, c, p) {
-  let valA = ptest(p[0], a, 'read');
-  let valB = ptest(p[1], b, 'read');
-  let valC = ptest(p[2], c, 'write');
+function opcode7 (a, b, c, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
+  let valB = ptest(p[1], b, 'read', program);
+  let valC = ptest(p[2], c, 'write', program);
 
   if (valA < valB) {
-    inputCopy[valC] = 1;
+    program[valC] = 1;
   } else {
-    inputCopy[valC] = 0;
+    program[valC] = 0;
   }
   // console.log(`op7: comparing if ${valA} is < ${valB}`);
 }
 
 // opcode 8 - if position 1 == position 2, position 3 is set to 1; otherwise, it's set to 0
-function opcode8 (a, b, c, p) {
-  let valA = ptest(p[0], a, 'read');
-  let valB = ptest(p[1], b, 'read');
-  let valC = ptest(p[2], c, 'write');
+function opcode8 (a, b, c, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
+  let valB = ptest(p[1], b, 'read', program);
+  let valC = ptest(p[2], c, 'write', program);
 
   if (valA == valB) {
-    inputCopy[valC] = 1;
+    program[valC] = 1;
   } else {
-    inputCopy[valC] = 0;
+    program[valC] = 0;
   }
   // console.log(`op8: comparing if ${valA} equals ${valB}`);
 }
 
 // opcode 9 - adjust relative base by the value given in position 1
-function opcode9 (a, p) {
-  let valA = ptest(p[0], a, 'read');
+function opcode9 (a, p, program) {
+  let valA = ptest(p[0], a, 'read', program);
   relBase += valA;
   // console.log(`op9: new relative base is ${relBase}`);
 }
 
 // allows parameter modes - 0 for position mode (value stored at position given); 1 for immediate mode (actual value listed); 2 for relative mode (value stored at position (actual value listed + the relative base))
-function ptest(param, checkval, mode) {
+function ptest(param, checkval, mode, program) {
   let returnVal;
   let baseVal = relBase + checkval;
 
   if (param == 0 || !param) {
     if (mode === 'read') {
-      returnVal = inputCopy[checkval];
+      returnVal = program[checkval];
     } else if (mode === 'write') {
       returnVal = checkval;
     }
@@ -111,7 +111,7 @@ function ptest(param, checkval, mode) {
     returnVal = checkval;
   } else if (param == 2) {
     if (mode === 'read') {
-      returnVal = inputCopy[baseVal];
+      returnVal = program[baseVal];
     } else if (mode === 'write') {
       returnVal = baseVal;
     }
@@ -119,10 +119,10 @@ function ptest(param, checkval, mode) {
 
   if (returnVal === undefined) {
     if (param == 0 || !param) {
-      inputCopy[checkval] = 0;
+      program[checkval] = 0;
       returnVal = 0;
     } else if (param == 2) {
-      inputCopy[baseVal] = 0;
+      program[baseVal] = 0;
       returnVal = 0;
     }
   }
@@ -134,41 +134,48 @@ function ptest(param, checkval, mode) {
 
 // run through memory input, following instructions until 99 is hit
 function runProgram(program, inputval) {
-  let inputCopy = [...program];
-  for (let i = 0; i < inputCopy.length; i++) {
-    if (inputCopy[i] === 99) {
+  let output = [];
+  let inputCounter = 0;
+
+  for (let i = 0; i < program.length; i++) {
+    if (program[i] === 99) {
       break;
     }
     
-    let instruct = inputCopy[i].toString();
+    let instruct = program[i].toString();
     let opval = parseInt(instruct.slice(-2), 10);
     let params = instruct.slice(0, -2).split('').reverse();
     
 
-    let ione = inputCopy[i+1];
-    let itwo = inputCopy[i+2];
-    let ithree = inputCopy[i+3];
+    let ione = program[i+1];
+    let itwo = program[i+2];
+    let ithree = program[i+3];
 
     switch (opval) {
       case 01:
-        opcode1(ione, itwo, ithree, params);
+        opcode1(ione, itwo, ithree, params, program);
         i += 3;
         break;
       case 02:
-        opcode2(ione, itwo, ithree, params);
+        opcode2(ione, itwo, ithree, params, program);
         i += 3;
         break;
       case 03:
-        opcode3(inputval, ione, params);
+        if (typeof inputval === 'number') {
+          opcode3(inputval, ione, params, program);
+        } else if (typeof inputval === 'object') {
+          opcode3(inputval[inputCounter], ione, params, program);
+          inputCounter++;
+        }
         i++;
         break;
       case 04:
-        let res = opcode4(ione, params);
-        console.log(res);
+        let res = opcode4(ione, params, program);
+        output.push(res);
         i++;
         break;
       case 05:
-        let checkt = opcode5(ione, itwo, i, params);
+        let checkt = opcode5(ione, itwo, i, params, program);
         if (i != checkt) {
           i = checkt - 1;
         } else {
@@ -176,7 +183,7 @@ function runProgram(program, inputval) {
         }
         break;
       case 06:
-        let checkf = opcode6(ione, itwo, i, params);
+        let checkf = opcode6(ione, itwo, i, params, program);
         if (i != checkf) {
           i = checkf - 1;
         } else {
@@ -184,19 +191,20 @@ function runProgram(program, inputval) {
         }
         break;
       case 07:
-        opcode7(ione, itwo, ithree, params);
+        opcode7(ione, itwo, ithree, params, program);
         i += 3;
         break;
       case 08:
-        opcode8(ione, itwo, ithree, params);
+        opcode8(ione, itwo, ithree, params, program);
         i += 3;
         break;
       case 09: 
-        opcode9(ione, params);
+        opcode9(ione, params, program);
         i++;
         break;
     }
   }
+  return output;
 }
 
-module.exports = runProgram();
+exports.runProgram = runProgram;
