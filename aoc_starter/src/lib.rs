d0207cc178e2b9e42794ea2ecad64a9162391dc4
Lindakat <lindakt16@gmail.com>
2020-11-21 00:04:45 -0600

    Files are correctly created and added! Working on opening file when done

--- aoc_starter/src/lib.rs
+++ aoc_starter/src/lib.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::error::Error;
-// use std::fs;
+use std::fs;
+use std::process::Command;
 
 // store our current day and puzzle input together
 pub struct Config {
@@ -20,10 +21,10 @@ impl Config {
             None => return Err("Please provide the day you're working on."),
         };
 
-        // not every day needs a separate input file, so save it if it's provided but otherwise that's ok
+        // not every day needs a separate input file, so make note of it one exists or not
         let puzzle_input = match args.next() {
             Some(arg) => arg,
-            None => "".to_string(),
+            None => return Err("Do you need a puzzle input file? Please say true or false"),
         };
 
         // return our new config struct
@@ -39,17 +40,69 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         "Config received: {}, {}",
         config.current_day, config.puzzle_input
     );
+
+    // create files
+    create_files(config);
+    // return ok to end function
     Ok(())
 }
 
-// setup puzzle input file
-// if it's not an empty string, confirm it's a url
-// if so, request the path and grab the data
-// make a new file in inputs folder named day##input.txt and write the data to it
-// make sure this only runs once!!
+// setup files
+pub fn create_files(config: Config) -> () {
+    println!("creating files!");
+    // make sure the day string is 2 digits - if not, add a 0 in front of what's provided (only time it won't be 2 digits is if it's 0-9)
+    let mut day: String = config.current_day;
+    if day.len() != 2 {
+        day = format!("{}{}", "0".to_string(), day);
+        println!("{}", day);
+    }
 
-// setup main day file
-// make sure the day string is 2 digits - if not, add a 0 in front of what's provided (only time it won't be 2 digits is if it's 0-9)
-// make a new file named day##.js
-// write the first few lines needed - require fs, and call to read and convert the input file to string (if input file exists)
-// if possible, open new file in editor
+    // make the input file, if input is true
+
+    let input_path: String = format!(
+        "{}{}{}",
+        "./2020 Solutions/inputs/day".to_string(),
+        day,
+        "input.txt".to_string()
+    );
+    if config.puzzle_input == "true" {
+        fs::File::create(&input_path).expect("Could not create input file.");
+    }
+    // make a new file named day##.js
+    let file_path: String = format!(
+        "{}{}{}",
+        "./2020 Solutions/day".to_string(),
+        day,
+        ".js".to_string()
+    );
+    // write the first few lines needed - require fs, and call to read and convert the input file to string (if input file exists)
+    let mut data_to_write: String = "".to_string();
+    if config.puzzle_input == "true" {
+        data_to_write = format!(
+            "{}{}{}",
+            "const fs = require('fs');
+            
+const data = fs.readFileSync('../inputs/day"
+                .to_string(),
+            day,
+            "input.txt').toString();
+            
+"
+        );
+    }
+    fs::write(&file_path, data_to_write).expect("Could not create day file");
+    // if possible, open new file in editor
+    let full_path: String = format!(
+        "{}{}{}",
+        "D:\\CodeFiles\\adventOfCode-JavaScript\\2020 Solutions\\day".to_string(),
+        day,
+        ".js".to_string()
+    );
+    edit::edit_file(&full_path).expect("Could not open file in editor");
+    // Command::new("$EDITOR")
+    //     .arg(&full_path)
+    //     .status()
+    //     .expect("Sorry, could not open file.");
+}
+
+// C:\\Users\\Linda\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code
