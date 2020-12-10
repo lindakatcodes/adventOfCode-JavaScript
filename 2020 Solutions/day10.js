7f47c1bbb3ecce74fd192a23d3ea3e63c780874a
Lindakat <lindakt16@gmail.com>
2020-12-10 14:29:22 -0600

    2020 day 10 pt 2 done with answer from reddit

--- 2020 Solutions/day10.js
+++ 2020 Solutions/day10.js
@@ -24,5 +24,24 @@ for (let i = 0; i < sortedAdapters.length; i++) {
   currVal = sortedAdapters[i];
 }
 
-console.log(diffs);
+// console.log(diffs);
 console.log(`part 1 answer: ${diffs.diff1 * diffs.diff3}`);
+
+// part 2 copied from u/barricuda - honestly still don't quite understand why it works, but it does
+sortedAdapters.unshift(0);
+sortedAdapters.push(sortedAdapters[sortedAdapters.length - 1] + 3);
+
+const poss = [1];
+
+function seq(index, diff) {
+  return sortedAdapters[index - diff] >= sortedAdapters[index] - 3 ? poss[index - diff] : 0;
+}
+
+for (let i = 1; i < sortedAdapters.length; i++) {
+  const count = seq(i, 1) + seq(i, 2) + seq(i, 3);
+  // console.log(count);
+  poss.push(count);
+}
+
+console.log(poss);
+console.log(poss[poss.length - 1]);
