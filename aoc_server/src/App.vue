39aff405ad18b361968112b57fd881b1edf4a055
LindaKat <lindakt16@gmail.com>
2021-11-10 23:22:11 -0600

    setup vite server project; debugged issues; added comments to readme

---
+++ aoc_server/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import '../../2018 Solutions/day08';
+</script>
+
+<template>
+  <p>Welcome to Advent of Code! Open the console to see today's tests.</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
