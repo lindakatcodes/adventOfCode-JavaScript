84123ee223b8abf7d6459b30d8182eaaa7062d41
LindaKat <lindakt16@gmail.com>
2021-12-15 21:27:05 -0600

    day 14 pt1

--- 2021 Solutions/day14.ts
+++ 2021 Solutions/day14.ts
@@ -2,17 +2,53 @@ import * as h from "../helpers";
 
 const data: string = await h.readData("./2021Inputs/day14input.txt");
 const splitData: string[] = h.strInput(data);
-const template: string[] = "NNCB".split("");
+const initialTemplate: string[] = "SHHNCOPHONHFBVNKCFFC".split("");
+let template: string[] = [...initialTemplate];
 
 const rules = new Map();
 
 splitData.forEach((rule: string) => {
-  const splitRule = rule.split(" -> ");
+  const splitRule = rule.split(" -> ").map((bit) => bit.trim());
   rules.set(splitRule[0], splitRule[1]);
 });
 
-console.log(rules);
+const letterCount = h.counts(initialTemplate);
 
-const step = 0;
+const steps = 10;
 
-for (let i = 0; i < 10; i++) {}
+function runStep(): string[] {
+  const newTemplate = [];
+  for (let i = 0; i < template.length; i += 1) {
+    if (i + 1 < template.length) {
+      const pairToMatch = `${template[i]}${template[i + 1]}`;
+      const newLetter: string = rules.get(pairToMatch);
+      letterCount[newLetter]
+        ? letterCount[newLetter]++
+        : (letterCount[newLetter] = 1);
+      i === 0
+        ? newTemplate.push(template[i], newLetter, template[i + 1])
+        : newTemplate.push(newLetter, template[i + 1]);
+    }
+  }
+  // console.log(newTemplate.length, letterCount);
+  return newTemplate;
+}
+
+for (let i = 0; i < steps; i++) {
+  const stepTemplate = runStep();
+  template = stepTemplate;
+}
+
+const sortedValues = Object.values(letterCount).sort(
+  (a, b) => (a as number) - (b as number)
+);
+const mostKey: string =
+  Object.keys(letterCount).find(
+    (key: string) => letterCount[key] === sortedValues[sortedValues.length - 1]
+  ) || "";
+const leastKey: string =
+  Object.keys(letterCount).find(
+    (key: string) => letterCount[key] === sortedValues[0]
+  ) || "";
+const minus = letterCount[mostKey] - letterCount[leastKey];
+console.log(minus);
